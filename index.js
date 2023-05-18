fc7839ccfb2fa3b4cb273f75cbfd0ba5d66014bb
ibrahim rabbi <ibrahimrabbihere@gmail.com>
2023-05-19 01:26:50 +0600

    delete api and query api added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,9 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const user = process.env.DATABASE_USER;
-const password = process.env.DATABASE_PASS
+const password = process.env.DATABASE_PASS;
 const uri = `mongodb+srv://${user}:${password}@cluster0.oqkryfl.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -26,26 +25,34 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-   
-      const dataCollaction = client.db('toysPark').collection('toys');
 
+    const dataCollaction = client.db("toysPark").collection("toys");
 
+    app.post("/toys", async (req, res) => {
+      const catchData = req.body;
+      const result = await dataCollaction.insertOne(catchData);
+      res.send(result);
+    });
 
-      app.post('/toys', async (req, res) => {  
-        const catchData = req.body;
-        const result = await dataCollaction.insertOne(catchData)
-        res.send(result); 
-      })
+    app.get("/toys", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const userData = dataCollaction.find(query);
+      const result = await userData.toArray();
+      res.send(result);
+    });
 
-    app.get('/toys', async (req, res) => {
-      const data = dataCollaction.find()
-      const result = await data.toArray()
-      res.send(result)
+    app.delete('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const remove = await dataCollaction.deleteOne(query)
+      res.send(remove)
     })
-       
-    
+
+
   } finally {
-    
   }
 }
 run().catch(console.dir);
