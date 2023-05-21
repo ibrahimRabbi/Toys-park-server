5bda0c8b2129cc479d11b67b5e37a8cfe5b720db
ibrahim rabbi <ibrahimrabbihere@gmail.com>
2023-05-21 19:35:19 +0600

    deploy related problem solve

--- index.js
+++ index.js
@@ -24,23 +24,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    //await client.connect();
     const dataCollaction = client.db("toysPark").collection("toys");
     const popularToyCollaction = client.db("toysPark").collection("popular");
 
-  //post insert api
+    //post insert api
     app.post("/toys", async (req, res) => {
       const catchData = req.body;
       const result = await dataCollaction.insertOne(catchData);
       res.send(result);
     });
 
-//query and all data and sorting data get api using email query params
+    //query and all data and sorting data get api using email query params
     app.get("/toys", async (req, res) => {
-
       let query = {};
       let sort = {};
-      
+
       if (req.query?.email) {
         query = { email: req.query.email };
       }
@@ -51,53 +50,52 @@ async function run() {
       const userData = dataCollaction.find(query).sort(sort);
       const result = await userData.toArray();
       res.send(result);
-
     });
 
-//limit 20 data get api
+    //limit 20 data get api
     app.get("/toyslimit", async (req, res) => {
       const userData = dataCollaction.find().limit(20);
       const result = await userData.toArray();
       res.send(result);
     });
 
-//category wise data get api
-    app.get('/category/:categoryName', async (req, res) => {
-      const categoryName = req.params.categoryName
-      const data = await dataCollaction.find().toArray()  
-      const categoryData = data.filter(v => v.category == categoryName)
-      res.send(categoryData)
-    })
+    //category wise data get api
+    app.get("/category/:categoryName", async (req, res) => {
+      const categoryName = req.params.categoryName;
+      const data = await dataCollaction.find().toArray();
+      const categoryData = data.filter((v) => v.category == categoryName);
+      res.send(categoryData);
+    });
 
-//name wise data get api for  search faild
-    app.get('/search/:toyName', async (req, res) => {
-      const toyName = req.params.toyName
-      const data = await dataCollaction.find().toArray()  
-      const categoryData = data.filter(v => v.toyName == toyName)
-      res.send(categoryData)
-    })
+    //name wise data get api for  search faild
+    app.get("/search/:toyName", async (req, res) => {
+      const toyName = req.params.toyName;
+      const data = await dataCollaction.find().toArray();
+      const categoryData = data.filter((v) => v.toyName == toyName);
+      res.send(categoryData);
+    });
 
-// one data get api using id
-    app.get('/toys/:id', async (req, res) => {
+    // one data get api using id
+    app.get("/toys/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const getOneData = await dataCollaction.findOne(query)
-      res.send(getOneData)
-    })
+      const query = { _id: new ObjectId(id) };
+      const getOneData = await dataCollaction.findOne(query);
+      res.send(getOneData);
+    });
 
-//popular toy data get api
-    app.get('/popular', async (re, res) => {
-      const data = await popularToyCollaction.find().toArray()
-      res.send(data)
-    })
-    
-//update put api
-    app.put('/update/:id', async (req, res) => {
+    //popular toy data get api
+    app.get("/popular", async (re, res) => {
+      const data = await popularToyCollaction.find().toArray();
+      res.send(data);
+    });
+
+    //update put api
+    app.put("/update/:id", async (req, res) => {
       const dataobj = req.body;
       const id = req.params.id;
-       
-      const filter = { _id: new ObjectId(id) }; 
-      const options = { upsert: true }; 
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateData = {
         $set: {
           toyName: dataobj.toyName,
@@ -106,22 +104,24 @@ async function run() {
           category: dataobj.category,
           stock: dataobj.stock,
           rating: dataobj.rating,
-          description : dataobj.description
+          description: dataobj.description,
         },
       };
-      const result = await dataCollaction.updateOne(filter, updateData, options)
-      res.send(result)
-    })
+      const result = await dataCollaction.updateOne(
+        filter,
+        updateData,
+        options
+      );
+      res.send(result);
+    });
 
-//delete api
-    app.delete('/toys/:id', async (req, res) => {
+    //delete api
+    app.delete("/toys/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const remove = await dataCollaction.deleteOne(query)
-      res.send(remove)
-    })
-
-
+      const query = { _id: new ObjectId(id) };
+      const remove = await dataCollaction.deleteOne(query);
+      res.send(remove);
+    });
   } finally {
   }
 }
